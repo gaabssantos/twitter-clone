aa330228bd6182893e542228d9c76d5455f9bbad
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-09-28 11:04:13 -0300

    feat(Listed my tweets): :sparkles:

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -7,11 +7,38 @@ use MF\Model\Container;
 
 class AppController extends Action {
     public function timeline() {
+        $this->checkUserLogged();
+
+        $tweet = Container::getModel('Tweet');
+        $tweet->__set('id_usuario', $_SESSION['id']);
+        $tweets = $tweet->getAll();
+
+        $this->view->tweets = $tweets;
+        $this->render('timeline');
+    }
+
+    public function tweet() {
+        $this->checkUserLogged();
+
+        $tweet = Container::getModel('Tweet');
+
+        $tweet->__set('tweet', $_POST['tweet']);
+        $tweet->__set('id_usuario', $_SESSION['id']);
+
+        if (!empty($tweet->__get('tweet'))) {
+            $tweet->save();
+
+            header('Location: /timeline?success');
+        } else {
+            header('Location: /timeline?error=tweet_empty');
+        }
+    }
+
+    function checkUserLogged() {
         session_start();
 
-        if($_SESSION['id'] != '' && $_SESSION['name'] != '') {
-            $this->render('timeline');
-        } else {
+        if((!isset($_SESSION['id']) || !$_SESSION['id'] != '') && 
+            (!isset($_SESSION['id']) || $_SESSION['name'] != '')) {
             header('Location: /?error=account_not_found');
         }
     }
---
+++ App/Models/Tweet.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use MF\Model\Model;
+
+class Tweet extends Model {
+    private $id;
+    private $id_usuario;
+    private $tweet;
+    private $date;
+
+    public function __get($name)
+    {
+        return $this->$name;
+    }
+
+    public function __set($name, $value)
+    {
+        $this->$name = $value;
+    }
+
+    public function save() {
+        $query = "insert into tweets (tweet, id_usuario) values (?, ?)";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(1, $this->__get('tweet'));
+        $stmt->bindValue(2, $this->__get('id_usuario'));
+        $stmt->execute();
+
+        return $this;
+    }
+
+    public function getAll() {
+        $query = 
+            "select 
+                t.id, t.id_usuario, t.tweet, u.name, DATE_FORMAT(t.date, '%d/%m/%Y %H:%i') as date
+            from 
+                tweets as t
+            left join
+                users as u 
+            on 
+                (t.id_usuario = u.id)
+            where 
+                id_usuario = ?
+            order by t.date desc";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(1, $this->__get('id_usuario'));
+        $stmt->execute();
+
+        $tweets = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+        return $tweets;
+    }
+}
+
+?>
