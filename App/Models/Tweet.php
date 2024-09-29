a51acf6541e13fe3d5cae4d5c82133ed62b8e9a2
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-09-29 16:17:28 -0300

    feat(Showing tweets from other users): :sparkles:

--- App/Models/Tweet.php
+++ App/Models/Tweet.php
@@ -33,7 +33,11 @@ class Tweet extends Model {
     public function getAll() {
         $query = 
             "select 
-                t.id, t.id_usuario, t.tweet, u.name, DATE_FORMAT(t.date, '%d/%m/%Y %H:%i') as date
+                t.id, 
+                t.id_usuario, 
+                t.tweet, 
+                u.name, 
+                DATE_FORMAT(t.date, '%d/%m/%Y %H:%i') as date
             from 
                 tweets as t
             left join
@@ -41,10 +45,13 @@ class Tweet extends Model {
             on 
                 (t.id_usuario = u.id)
             where 
-                id_usuario = ?
+                t.id_usuario = ?
+            or
+                t.id_usuario in (select id_usuario_following from users_followers where id_usuario = ?)
             order by t.date desc";
         $stmt = $this->db->prepare($query);
         $stmt->bindValue(1, $this->__get('id_usuario'));
+        $stmt->bindValue(2, $this->__get('id_usuario'));
         $stmt->execute();
 
         $tweets = $stmt->fetchAll(\PDO::FETCH_ASSOC);
